511c237637af30d26502622f210af55bbc78d8ac
Stepan080713 <stprs61636@gmail.com>
2026-09-22 12:16:44 +0300

    Практическая №3

---
+++ Задание_4.py
@@ -0,0 +1,19 @@
+kolvo_students = 29
+temperature = 28.5
+Login_user = "Ivanov Ivan"
+yvedomlenie = False
+Films = ["Matrix","Django Unchained","Evil Dead","Terminator"]
+kordinat_2D = (30,50)
+items_id = (666,333,222,515,7768,1413)
+country = {
+    "Russia" : "Moscow",
+    "France" : "Paris",
+    "United Kingdom" : "London"
+}
+print(kolvo_students)
+print(temperature)
+print(Login_user)
+print(yvedomlenie)
+print(Films)
+print(kordinat_2D)
+print(items_id)
---
+++ Задание_5.py
@@ -0,0 +1,7 @@
+Shop_list = ["Хлеб","Морковь","Молоко","Кетчуп","Мазик"]
+Shop_list.append("яблоки",)
+
+print(Shop_list)
+
+mnojestvo_items = set(Shop_list)
+print(mnojestvo_items)
